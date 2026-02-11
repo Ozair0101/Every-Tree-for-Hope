3252573d21b532cd2079eae7d94681bf3c2616b0
Ozair0101 <ozairjan186@gmail.com>
2026-02-11 12:09:46 +0430

    sadf

---
+++ database/migrations/2026_02_11_071411_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->date('date');
+            $table->integer('trees_planted')->default(0);
+            $table->integer('volunteers')->default(0);
+            $table->string('sponsor_partner')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
