f6907f2799beeac40b089d87a4c31c2e44a9ca0f
Ozair0101 <ozairjan186@gmail.com>
2026-02-12 16:26:29 +0430

    asdf

--- app/Filament/Resources/Donators/Pages/CreateDonator.php
+++ app/Filament/Resources/Donators/Pages/CreateDonator.php
@@ -8,4 +8,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateDonator extends CreateRecord
 {
     protected static string $resource = DonatorResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
