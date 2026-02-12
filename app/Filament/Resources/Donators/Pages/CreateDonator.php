7d01c159242cc27970f212ab94205edb5bd980e7
Ozair0101 <ozairjan186@gmail.com>
2026-02-12 16:15:27 +0430

    sadf

---
+++ app/Filament/Resources/Donators/Pages/CreateDonator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Donators\Pages;
+
+use App\Filament\Resources\Donators\DonatorResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDonator extends CreateRecord
+{
+    protected static string $resource = DonatorResource::class;
+}
