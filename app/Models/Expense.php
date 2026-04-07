cd4cd7aa536b6fdbeeaca4c5ec945c713436df66
Ozair0101 <ozairjan186@gmail.com>
2026-04-07 14:22:55 +0430

    asdf

---
+++ app/Models/Expense.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    protected $fillable = [
+        'date',
+        'description',
+        'quantity',
+        'unit_price',
+        'total_cost',
+        'who_paid',
+        'notes',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'unit_price' => 'decimal:2',
+        'total_cost' => 'decimal:2',
+    ];
+
+    public function scopeOrdered($query)
+    {
+        return $query->orderBy('date', 'desc');
+    }
+}
