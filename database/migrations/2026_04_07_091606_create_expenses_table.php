67cde8cfda9122bcdd865075449d3e0fdd1c9a0f
Ozair0101 <ozairjan186@gmail.com>
2026-04-07 14:23:29 +0430

    asdf

---
+++ database/migrations/2026_04_07_091606_create_expenses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('description');
+            $table->string('quantity')->nullable();
+            $table->decimal('unit_price', 12, 2)->default(0);
+            $table->decimal('total_cost', 12, 2)->default(0);
+            $table->string('who_paid')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
