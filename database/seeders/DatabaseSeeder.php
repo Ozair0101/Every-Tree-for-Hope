36b1a1769438f8b473454713ca59f65e8ffa010f
Ozair0101 <ozairjan186@gmail.com>
2026-04-16 10:47:25 +0430

    adsf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             PartnerSeeder::class,
             MediaSeeder::class,
             ContactMessageSeeder::class,
+            FaqSeeder::class,
         ]);
     }
 }
