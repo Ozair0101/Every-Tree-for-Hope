7475b745f96ce453fbf01a1ec18b4698e0751587
Ozair0101 <ozairjan186@gmail.com>
2026-05-09 12:14:05 +0430

    asdf

---
+++ app/Filament/Resources/InvolvementRequestResource/Pages/CreateInvolvementRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\InvolvementRequestResource\Pages;
+
+use App\Filament\Resources\InvolvementRequestResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateInvolvementRequest extends CreateRecord
+{
+    protected static string $resource = InvolvementRequestResource::class;
+}
