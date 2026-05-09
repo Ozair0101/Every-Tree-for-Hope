9306e8300bdbf39d4e4002566199dd3cea45c292
Ozair0101 <ozairjan186@gmail.com>
2026-05-09 12:15:58 +0430

    asdf

---
+++ app/Filament/Resources/TreeRequestResource/Pages/CreateTreeRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\TreeRequestResource\Pages;
+
+use App\Filament\Resources\TreeRequestResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTreeRequest extends CreateRecord
+{
+    protected static string $resource = TreeRequestResource::class;
+}
