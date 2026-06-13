01428df1c0dab49c5416e8ae77f7fdd0c3c413e5
Ozair0101 <ozairjan186@gmail.com>
2026-06-13 10:13:13 +0430

    add carear

---
+++ app/Filament/Resources/JobApplications/Pages/EditJobApplication.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\JobApplications\Pages;
+
+use App\Filament\Resources\JobApplications\JobApplicationResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditJobApplication extends EditRecord
+{
+    protected static string $resource = JobApplicationResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            ViewAction::make(),
+            DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
