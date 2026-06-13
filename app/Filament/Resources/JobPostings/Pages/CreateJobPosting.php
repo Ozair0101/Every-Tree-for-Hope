ac8500388bde71a8244a0e2fa6f9b5c5172e1f3f
Ozair0101 <ozairjan186@gmail.com>
2026-06-13 10:13:34 +0430

    add carear

---
+++ app/Filament/Resources/JobPostings/Pages/CreateJobPosting.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\JobPostings\Pages;
+
+use App\Filament\Resources\JobPostings\JobPostingResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateJobPosting extends CreateRecord
+{
+    protected static string $resource = JobPostingResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
