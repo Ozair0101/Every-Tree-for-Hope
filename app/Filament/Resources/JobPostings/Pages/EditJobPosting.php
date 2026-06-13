2b07095690996ef3529d882b961094d0cfd4efab
Ozair0101 <ozairjan186@gmail.com>
2026-06-13 10:13:36 +0430

    add carear

---
+++ app/Filament/Resources/JobPostings/Pages/EditJobPosting.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\JobPostings\Pages;
+
+use App\Filament\Resources\JobPostings\JobPostingResource;
+use Filament\Actions\DeleteAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditJobPosting extends EditRecord
+{
+    protected static string $resource = JobPostingResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
